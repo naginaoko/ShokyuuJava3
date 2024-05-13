98b2e33df62d384dafadc55bb4f05623284732d6
naginaoko <zeal23410186@fsgcl.jp>
2024-05-13 11:55:06 +0900

    初級Java2.5 プロジェクトのコミット

---
+++ 初級Java2.5/src/Start.java
@@ -0,0 +1,12 @@
+
+public class Start {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		MyFrame frame1=new RectFrame();
+		frame1.setLocation(50,100);
+		MyFrame frame2=new RectFrame();
+		frame2.setLocation(300,100);
+	}
+
+}
---
+++ 初級Java2.5/src/practice2_1.java
@@ -0,0 +1,12 @@
+
+public class practice2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int x=2;
+		int y=3;
+		System.out.println(x*y);
+
+	}
+
+}
