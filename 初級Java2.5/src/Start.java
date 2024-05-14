bc3bc818d47ea2398765213601d2159a26a1f8a2
naginaoko <zeal23410186@fsgcl.jp>
2024-05-14 10:46:11 +0900

    Java3.1章のコミット

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,10 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		MyFrame frame1=new RectFrame();
-		frame1.setLocation(50,100);
-		MyFrame frame2=new RectFrame();
-		frame2.setLocation(300,100);
+		System.out.println("プログラムを実行します");
+		MyFrame frame1=new RectTest();
 	}
 
 }
