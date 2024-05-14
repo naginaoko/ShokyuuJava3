7e07e3cbd74c15016c8dd8e66e694422c0111ad8
naginaoko <zeal23410186@fsgcl.jp>
2024-05-14 12:11:12 +0900

    Java3.3章のコミット

--- 初級Java2.5/src/RectTest.java
+++ 初級Java2.5/src/RectTest.java
@@ -1,10 +1,21 @@
 
 public class RectTest extends MyFrame{
 	public void run() {
+		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		
+		setColor(148,87,164);
 		fillRect(130,40,50,100);
+		
+		setColor(24,235,255);
 		fillRect(30,200,150,10);
+		
+		setColor(0,0,0);
 		fillRect(90,160,30,30);
+		
+		setColor(128,128,128);
+		fillRect(160,300,150,10);
+		
 	}
 
 }
