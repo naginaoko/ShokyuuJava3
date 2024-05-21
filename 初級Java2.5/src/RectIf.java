ee816d5e341b13e5dae92a93408dd70189ab4e40
naginaoko <zeal23410186@fsgcl.jp>
2024-05-21 11:44:07 +0900

    追加課題その1

--- 初級Java2.5/src/RectIf.java
+++ 初級Java2.5/src/RectIf.java
@@ -1,18 +1,17 @@
 
 public class RectIf extends MyFrame{
 	public void run() {
-		int x=30;
-		int r=0;
-		int g=0;
-		int b=0;
-		setColor(r,g,b);
-		while(x<=210) {
-			fillRect(x,80,10,100);
-			x+=20;
-			r+=20;
-			g+=20;
-			b+=20;
-			setColor(r,g,b);
+		int x=-10;
+		int y=-10;
+		int w=10;
+		int h=10;
+		setColor(0,128,0);
+		while(x<=410) {
+			fillRect(x,y,w,h);
+			x+=w;
+			y+=h;
+			w+=10;
+			h+=10;
 		}
 	}
 
