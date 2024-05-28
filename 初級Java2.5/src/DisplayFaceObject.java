5116310c03e63fc4e8f8ce3cb008cda3fc65e6c7
naginaoko <zeal23410186@fsgcl.jp>
2024-05-28 10:12:42 +0900

    確認問題6-2

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,11 +1,14 @@
 
 public class DisplayFaceObject extends MyFrame{
 	public void run() {
-		Face face1=new Face(50,50);
+		Face face1=new Face(20,30);
 		face1.draw(this);
 		
-		Face face2=new Face(200,100);
+		Face face2=new Face(220,30);
 		face2.draw(this);
+		
+		Face face3=new Face(120,130);
+		face3.draw(this);
 	}
 
 }
